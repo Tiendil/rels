428126684e9650aabdaab27bd1f54d92b0b44c16
Tiendil <a.eletsky@gmail.com>
2013-02-25 21:19:51 +0300

    add setup configs

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import setuptools
+
+setuptools.setup(
+    name = 'Rels',
+    version = '0.1.0',
+    author = 'Aleksey Yeletsky',
+    author_email = 'a.eletsky@gmail.com',
+    packages = setuptools.find_packages(),
+    url = 'https://github.com/Tiendil/rels',
+    license = 'LICENSE',
+    description = 'library for describing data relations in python (Enums, "relational" tables)',
+    long_description = open('README').read(),
+    include_package_data = True # setuptools-git MUST be installed
+)
