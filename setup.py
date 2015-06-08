32e905c30401f2acac1e1a15b788519182e4646d
Tiendil <a.eletsky@gmail.com>
2015-06-08 11:25:08 +0300

    gh-10 Сформировать пакет и выложить в PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,30 @@
 import setuptools
 
 setuptools.setup(
-    name = 'Rels',
-    version = '0.2.0',
-    author = 'Aleksey Yeletsky',
-    author_email = 'a.eletsky@gmail.com',
-    packages = setuptools.find_packages(),
-    url = 'https://github.com/Tiendil/rels',
-    license = 'LICENSE',
-    description = 'library for describing data relations in python (Enums, "relational" tables)',
-    long_description = open('README.md').read(),
-    include_package_data = True # setuptools-git MUST be installed
-)
+    name='Rels',
+    version='0.2.0',
+    description='Library for describing data relations in python (Enums, "relational" tables)',
+    long_description = open('README.rst').read(),
+    url='https://github.com/Tiendil/rels',
+    author='Aleksey Yeletsky <Tiendil>',
+    author_email='a.eletsky@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Natural Language :: English',
+        'Natural Language :: Russian'],
+    keywords=['enums', 'data relations', 'relational tables'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    test_suite = 'tests',
+    )
